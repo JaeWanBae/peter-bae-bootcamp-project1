b8211dc4f44a5f5fbd6705764561509889f057bf
Peter.JaeWan.Bae <peter.jae.wan.bae@gmail.com>
2021-03-20 19:01:31 -0400

    add JS for hiding and showing burger menu

---
+++ scripts.js
@@ -0,0 +1,15 @@
+// interactive burger menu 
+// when clicked toggle to show menu. 
+
+const navElement = document.querySelector(`nav`)
+
+navElement.addEventListener(`click`, function(event) {
+    console.log(event.target.tagName)
+    if(event.target.tagName === `I`){
+        showBurgerNav(event.target);
+    }
+})
+
+function showBurgerNav(nav) {
+    nav.parentElement.classList.toggle(`burgerNavShow`)
+}
